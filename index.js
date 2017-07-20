1b9411fc6efffe1a8422908e3a83533d3017df4b
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-20 19:07:55 -0400

    turned on the cors with the middleware

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const port = process.env.PORT || 8080;
 
 // middleware imports
 const morgan = require('morgan');
+const cors = require('cors');
 
 // wire up the middleware
+server.use(cors());
 server.use(morgan('dev'));
+
 
 server.get('/', (req, res) => {
      res.send('it works');
